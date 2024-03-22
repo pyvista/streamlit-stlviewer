2d373c02c62e852d7a27dfbd33a696bb1a1a276f
Tetsuo Koyama <tkoyama010@gmail.com>
2024-03-23 00:36:11 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,42 +2,26 @@ import streamlit as st
 import pyvista as pv
 from pyvista import examples
 
-## Using pythreejs as pyvista backend
 pv.set_jupyter_backend('pythreejs')
 
-## Download an stl file
-# filename = examples.download_cad_model(load=False)
 uploadedFile = st.file_uploader("Upload a STL:",["stl"],False)
 
-## Streamlit layout
 st.sidebar.title("STL viewer")
 
 if uploadedFile:
-    
     stlTemp = "./temp.stl"
     with open(stlTemp, "wb") as f: 
         f.write(uploadedFile.getbuffer())
-
-    color = st.sidebar.selectbox("Pick a color:",["white","green","blue"])
-
-    ## Initialize pyvista reader and plotter
     reader = pv.STLReader(stlTemp)
     plotter = pv.Plotter(
         border=True,
         window_size=[580,400]) 
+    color = st.sidebar.selectbox("Pick a color:",["white","green","blue"])
     plotter.background_color = "white"
-
-    ## Read data and send to plotter
     mesh = reader.read()
-    plotter.add_mesh(mesh,color=color)
-
-    ## Export to an external pythreejs
+    plotter.add_mesh(mesh, color=color)
     model_html = "model.html"
     other = plotter.export_html(model_html, backend='pythreejs')
-
-    ## Read the exported model
     with open(model_html,'r') as file: 
         model = file.read()
-
-    ## Show in webpage
     st.components.v1.html(model,height=600, width=600)
