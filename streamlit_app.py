74c29b911cb324ef8c5b6999a7af25065a130289
Tetsuo Koyama <tkoyama010@gmail.com>
2024-03-23 10:31:55 +0900

    Update `streamlit_app.py` to remove unused indent

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,13 +12,13 @@ if uploaded_file:
         reader = pv.STLReader(fp.name)
         mesh = reader.read()
 
-    color = st.sidebar.selectbox("Pick a color:", ["white", "green", "blue"])
-    plotter = pv.Plotter(border=True, window_size=[580, 400])
-    plotter.background_color = "white"
-    plotter.add_mesh(mesh, color=color)
+color = st.sidebar.selectbox("Pick a color:", ["white", "green", "blue"])
+plotter = pv.Plotter(border=True, window_size=[580, 400])
+plotter.background_color = "white"
+plotter.add_mesh(mesh, color=color)
 
-    with tempfile.NamedTemporaryFile(suffix=".html") as fp:
-        other = plotter.export_html(fp.name, backend="pythreejs")
-        with open(fp.name, "r") as f:
-            model = f.read()
-            st.components.v1.html(model, height=600, width=600)
+with tempfile.NamedTemporaryFile(suffix=".html") as fp:
+    other = plotter.export_html(fp.name, backend="pythreejs")
+    with open(fp.name, "r") as f:
+        model = f.read()
+        st.components.v1.html(model, height=600, width=600)
