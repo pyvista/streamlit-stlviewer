5ab5dd7d92a720bf1df58c73a7bf798869bc23d2
Tetsuo Koyama <tkoyama010@gmail.com>
2024-03-23 09:34:18 +0900

    Update .pre-commit-config.yaml

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,26 @@
+import tempfile
 import streamlit as st
 import pyvista as pv
-from pyvista import examples
 
-pv.set_jupyter_backend('pythreejs')
+pv.set_jupyter_backend("pythreejs")
 
-uploaded_file = st.file_uploader("Upload a STL:",["stl"],False)
+uploaded_file = st.file_uploader("Upload a STL:", ["stl"], False)
 
 st.sidebar.title("STL viewer")
 
 if uploaded_file:
-    color = st.sidebar.selectbox("Pick a color:",["white","green","blue"])
-    stlTemp = "./temp.stl"
-    with open(stlTemp, "wb") as f: 
-        f.write(uploaded_file.getbuffer())
-    reader = pv.STLReader(stlTemp)
-    plotter = pv.Plotter(
-        border=True,
-        window_size=[580,400]) 
+    with tempfile.NamedTemporaryFile(suffix=".stl") as fp:
+        fp.write(uploaded_file.getbuffer())
+        reader = pv.STLReader(fp.name)
+        mesh = reader.read()
+
+    color = st.sidebar.selectbox("Pick a color:", ["white", "green", "blue"])
+    plotter = pv.Plotter(border=True, window_size=[580, 400])
     plotter.background_color = "white"
-    mesh = reader.read()
     plotter.add_mesh(mesh, color=color)
-    model_html = "model.html"
-    other = plotter.export_html(model_html, backend='pythreejs')
-    with open(model_html,'r') as file: 
-        model = file.read()
-    st.components.v1.html(model,height=600, width=600)
+
+    with tempfile.NamedTemporaryFile(suffix=".html") as fp:
+        other = plotter.export_html(fp.name, backend="pythreejs")
+        with open(fp.name, "r") as f:
+            model = f.read()
+            st.components.v1.html(model, height=600, width=600)
